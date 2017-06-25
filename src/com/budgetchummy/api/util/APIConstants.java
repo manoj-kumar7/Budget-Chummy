e2742f18ce55e613d1e745b43e74c66d6f680878
manoj_kumar_a <manojskct@gmail.com>
2017-06-25 17:27:31 +0530

    started openshift branch and put only the war file in it

--- src/com/budgetchummy/api/util/APIConstants.java
+++
@@ -1,7 +0,0 @@
-package com.budgetchummy.api.util;
-
-public interface APIConstants {
-	public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/";
-	public static final String MYSQL_USERNAME = "root";
-	public static final String MYSQL_PASSWORD = "manoj";
-}
